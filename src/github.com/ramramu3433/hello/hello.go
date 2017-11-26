3cd5c50a6fea99ced934b373f4a8bf4b0cd1f087
ramramu3433 <ramramu.jr@gmail.com>
2017-11-26 20:47:20 +0530

    Dev practice 2 - datatype (math) ---same as c and java --formatting also

--- src/github.com/ramramu3433/hello/hello.go
+++ src/github.com/ramramu3433/hello/hello.go
@@ -5,5 +5,10 @@ import "fmt"
 func main(){
 
 fmt.Printf("Hello world \t Google developed this lang \n");
+fmt.Println("Here i am writing some basic math ")
+var i,j int
+i=10;
+j=20;
+fmt.Printf("The value of x+y is %d \n",i+j)
 }
 
